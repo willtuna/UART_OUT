c006adfd13ebcbb8a081f983dde10c2c2fd1c96c
DRONE <willvegapunk@gmail.com>
2016-10-07 13:55:18 +0000

    Update

--- uart_exe_arm_test.cpp
+++ uart_exe_arm_test.cpp
@@ -336,7 +336,8 @@ int main(int argc, char const *argv[])
 			confirm++;
 			arm = 0;
 	}
-	sleep(10);
+	
+	if(confirm >0 && arm == 0 )sleep(10);
 	if(rcv_count > 1000 && confirm >0 && arm == 0){
     		mavlink_msg_command_long_pack( 0 , 0, &msg_send, sysid , compid , MAV_CMD_DO_SET_MODE , confirm , 	MAV_MODE_STABILIZE_ARMED,	MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 0, 0, 0, 0, 0);
     		printf("Write %d bytes\n",serial_port.write_message(msg_send));

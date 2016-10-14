7497dad5f50ae90c64930cfdb08eb12452f4c139
willvegapunk <willvegapunk@gmail.com>
2016-10-14 16:26:50 +0800

    Debug

--- uart_exe_arm_test.cpp
+++ uart_exe_arm_test.cpp
@@ -348,7 +348,15 @@ int main(int argc, char const *argv[])
 			confirm++;
 			arm = 0;
 	}
-	//if(confirm >0 && arm == 0 )sleep(10);
+	if(confirm >0 && arm == 0 )sleep(10);
+
+		if(confirm  > 0 ){
+    		mavlink_msg_command_long_pack( 0 , 0, &msg_send, sysid , compid ,MAV_CMD_DO_SET_MODE , confirm , 	MAV_MODE_STABILIZE_ARMED , 0, 0, 0, 0, 0, 0);
+    		printf("Write %d bytes\n",serial_port.write_message(msg_send));
+		    printf("ARM Executed !!\n");
+			
+			arm = 1;
+	}
 	/*
 	if(rcv_count == 1000 && confirm >0 && arm == 0){
     		mavlink_msg_command_long_pack( 0 , 0, &msg_send, sysid , compid , MAV_CMD_NAV_LAND, confirm , 0 , 0, 0,0,ptr-> lat,ptr-> lon,ptr-> alt);

6ae7b21a9c406f023c9d99df789dffc3daa859e9
willvegapunk <willvegapunk@gmail.com>
2017-02-05 14:41:35 +0800

    change to 1200 and stabilize mode

--- vega_c_uart/mavlink_control.cpp
+++ vega_c_uart/mavlink_control.cpp
@@ -237,7 +237,7 @@ commands(Autopilot_Interface &api,float dx,float dy,float dz)
        
         mavlink_message_t msg_send;
        
-        mavlink_msg_rc_channels_override_pack( 0 , 0 , & msg_send, api.current_messages.sysid, api.current_messages.compid, UINT16_MAX , UINT16_MAX , 1800 , UINT16_MAX,UINT16_MAX, UINT16_MAX,UINT16_MAX,UINT16_MAX );
+        mavlink_msg_rc_channels_override_pack( 0 , 0 , & msg_send, api.current_messages.sysid, api.current_messages.compid, UINT16_MAX , UINT16_MAX , 1200 , UINT16_MAX,UINT16_MAX, UINT16_MAX,UINT16_MAX,UINT16_MAX );
         printf("Write %d bytes\n", serial_port.write_message(msg_send) );
         printf("RC overwrite !!");
 

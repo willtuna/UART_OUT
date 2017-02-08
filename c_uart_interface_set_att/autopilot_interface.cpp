f017d28433d37642889dfbd76ad31d7a47eddadd
willvegapunk <willvegapunk@gmail.com>
2017-02-08 21:56:15 +0800

    print for debug

--- c_uart_interface_set_att/autopilot_interface.cpp
+++ c_uart_interface_set_att/autopilot_interface.cpp
@@ -823,7 +823,7 @@ write_thread(void)
         target_attitude.body_roll_rate =  0;
         target_attitude.body_pitch_rate = 0;
         target_attitude.body_yaw_rate = 0;
-        float throttle = 0.6;
+        float throttle = 0.65;
         float pitchspeed = 0.5; // does'nt matter
         int count = 0;
         int rem=0; // remainder for oscillation
@@ -870,10 +870,10 @@ write_thread(void)
 //  Current focus on alt-hold
                 target_attitude.time_boot_ms =  (uint32_t)(get_time_usec()/1000);
         
-                target_attitude.body_roll_rate =  0; //current_messages.attitude.rollspeed;
+                target_attitude.body_roll_rate = current_messages.attitude.rollspeed;
                 
-                target_attitude.body_pitch_rate = 0; //pitchspeed;
-                target_attitude.body_yaw_rate = 0; //current_messages.attitude.yawspeed;
+                target_attitude.body_pitch_rate = current_messages.attitude.pitchspeed;
+                target_attitude.body_yaw_rate = current_messages.attitude.yawspeed;
 
                 if(current_messages.local_position_ned.z < initial_position.z ){
                     if(current_messages.local_position_ned.vz <  0)
@@ -884,7 +884,7 @@ write_thread(void)
                         throttle += 0.05;
                     }
                 }
-
+                printf("local_pos: %f   initial_ps: %f,  throttle: %f \n",current_messages.local_position_ned.z, initial_position.z, throttle);
 
 
 
